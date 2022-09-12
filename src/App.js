a38d34b6876b22cf19f3109fa4d76ddce6a81c34
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-12 15:04:08 -0700

    navbar routes udated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/navbar/Footer';
 import Navbar from './components/navbar/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />{' '}
+            {/*for not found pages */}
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
