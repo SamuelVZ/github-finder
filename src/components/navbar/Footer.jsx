9ad7d7788c49b2f327099ef719251bff9239ff68
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-12 15:03:30 -0700

    footer component created

---
+++ src/components/navbar/Footer.jsx
@@ -0,0 +1,20 @@
+import { BsGithub } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
+      <div>
+        <BsGithub className='text-5xl' />
+        <a
+          href='https://github.com/SamuelVZ'
+          className='text-lg align-middle font bold'
+        >
+          https://github.com/SamuelVZ
+        </a>
+        <p>Copyright &copy; {year} All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
