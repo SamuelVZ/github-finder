8907032a47e2a09f6cc41bfbd5925581fe546b12
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-30 12:14:09 -0700

    alert and user path added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Alert from './components/layout/Alert';
 import Footer from './components/navbar/Footer';
 import Navbar from './components/navbar/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 import NotFound from './components/pages/NotFound';
+import User from './components/users/User';
 import { AlertProvider } from './context/alert/AlertContext';
 import { GithubProvider } from './context/github/GithubContex';
 
@@ -15,9 +17,11 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
-                <Route exact path='/' element={<Home />} />
+                <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />{' '}
                 {/*for not found pages */}
